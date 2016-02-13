d4db72d37a07fab98c73c567db468f3825ff007b
Albert Siddhartha Slawinski <errendir@gmail.com>
2016-02-14 00:44:52 +0100

    Add correct versions to better understand the compilation differences

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const fun = async (a,b='',c) => {
+const fun0 = (a,b='',c) => {
   console.log("Called with",a,b,c);
   return null;
 }
 
-fun(1,2,3);
+const fun1 = async (a,b='',c) => {
+  console.log("Called with",a,b,c);
+  return null;
+}
+
+const fun2 = async function(a,b='',c) {
+  console.log("Called with",a,b,c);
+  return null;
+}
+
+fun0(1,2,3);
+fun1(1,2,3);
+fun2(1,2,3);
